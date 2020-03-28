2058968ef993df479dcf7563bb20a6f0029cac0b
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-28 16:24:38 +0530

    Insert Method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -7,6 +7,7 @@
 package problem5.circularqueue;
 
 import problem5.node.Node;
+import problem5.student.Student;
 
 //to implement circular queue
 public class MyCircularQueue {
@@ -16,4 +17,18 @@ public class MyCircularQueue {
         rear = front = null;
     }
 
+    public void insert(Student student) {
+        if (student.getAppearing() > 2)
+            System.out.println("CANNOT INSERT THIS STUDENT --> " + student.getName());
+        else {
+            Node n = new Node(student);
+            if (front == null) {
+                front = n;
+            } else
+                rear.setNext(n);
+            rear = n;
+            rear.setNext(front);
+
+        }
+    }
 }
