17150f643e032363dc9fc56766cbcdf9b5709034
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-28 18:29:30 +0530

    Created Add-Last Method

--- src/problem4/linklist/MyLinkList.java
+++ src/problem4/linklist/MyLinkList.java
@@ -3,4 +3,11 @@ package problem4.linklist;
 public class MyLinkList {
     private Node first;
     private Node last;
-}
+
+    public void addLast(int data) {
+        Node node = new Node(data);
+        if (first == null) {
+            first = last = node;
+            return;
+        }
+    }
