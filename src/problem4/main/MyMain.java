13826039620ca87fb4f8e8e7d02aeab6d0fe8c8a
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-28 18:43:50 +0530

    Main Method Created

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,9 +5,25 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem4.binaryST.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(myQueue);
+        myBinarySearchTree.insert(1001);
+        myBinarySearchTree.insert(200);
+        myBinarySearchTree.insert(30);
+        myBinarySearchTree.insert(2);
+        myBinarySearchTree.insert(10);
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder successors of all the nodes");
+        myQueue.show();
 
+    }
 
 }
