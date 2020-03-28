00b87bfe2b9239107be6fab48f06c391fac014ce
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-28 15:55:26 +0530

    Values Inserted in tree

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        bst.insert(85);
+        bst.insert(25);
+        bst.insert(45);
+        bst.insert(15);
+        bst.insert(17);
+        bst.insert(15);
+        bst.insert(10);
+    }
 }
