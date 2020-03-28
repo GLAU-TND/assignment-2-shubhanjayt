c12bb011669bf35b95386bca66e5054d9568d0bc
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-28 18:37:35 +0530

    Created enqueue,dequeue and show method

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,28 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.linklist.MyLinkList;
+
 public class MyQueue {
+
+    MyLinkList linkedList = new MyLinkList();
+
+    public int peek(int data) {
+        if (linkedList.search(data) == null) {
+            return -1;
+        }
+        return linkedList.search(data).getData();
+    }
+
+    public void enqueue(int data) {
+        linkedList.addLast(data);
+    }
+
+    public void dequeue() {
+        linkedList.deleteFirst();
+    }
+
+    public void show() {
+        linkedList.display();
+    }
 }
