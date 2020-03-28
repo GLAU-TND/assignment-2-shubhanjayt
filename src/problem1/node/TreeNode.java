fa66d3906149363d2574553170b15c3e1898c1f6
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-28 15:41:49 +0530

    Getter and Setter

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -14,5 +14,30 @@ public class TreeNode {
         this.data = data;
         this.left = this.right = null;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
+
 
