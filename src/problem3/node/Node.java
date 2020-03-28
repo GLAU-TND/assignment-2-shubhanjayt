c54d631f8d0ae642721123622653d351ccf0fdee
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-28 16:09:54 +0530

    Getter and Setter

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -16,4 +16,29 @@ public class Node {
         this.roll_no = roll_no;
         this.next = null;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll_no() {
+        return roll_no;
+    }
+
+    public void setRoll_no(int roll_no) {
+        this.roll_no = roll_no;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
+
